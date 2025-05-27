abc78e99c17dfe3e49c9b0ee2303003595c553c1
Hagi T <marspeoplehg@gmail.com>
2025-05-27 12:09:29 +0900

    Initial Commit

---
+++ AllDoneToDo/AllDoneToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllDoneToDoApp.swift
+//  AllDoneToDo
+//
+//  Created by Tomohiro Hagino on 2025/05/27.
+//
+
+import SwiftUI
+
+@main
+struct AllDoneToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AllDoneToDoTests/AllDoneToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AllDoneToDoTests.swift
+//  AllDoneToDoTests
+//
+//  Created by Tomohiro Hagino on 2025/05/27.
+//
+
+import Testing
+@testable import AllDoneToDo
+
+struct AllDoneToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
